ff8f853343ac2a433f08d274d99f68ac8cab022a
SrHoustX <111614312+SrHoustX@users.noreply.github.com>
2022-08-19 19:57:59 -0300

    wlc.py

---
+++ wlc.py
@@ -0,0 +1,10 @@
+import os,sys,time,random
+print("")
+print("")
+color = ["\033[1;31m"]
+m = "\033[1;31m"+"Seja bem-vindo MaTreX.... Aproveite ;) \n"
+for msg in m:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    time.sleep(0.06)
+print("")
